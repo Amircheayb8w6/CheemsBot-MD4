e5cd65dea0d5700013eb0d8c92d3398d9074a15a
Amircheayb8w6 <109865558+Amircheayb8w6@users.noreply.github.com>
2022-07-23 19:23:32 +0300

    Update settings.js

--- settings.js
+++ settings.js
@@ -27,7 +27,7 @@ const chalk = require('chalk')
 //Api Website\\
 global.APIs = {
 	zenz: 'https://zenzapis.xyz',
-}
+
 
 //Api Key\\
 global.APIKeys = {
@@ -54,23 +54,23 @@ global.doc5 = 'application/pdf'
 global.doc6 = 'application/vnd.android.package-archive'
 
 //owmner v card
-global.vcardowner = ['916909137213'] //ur owner number
-global.ownername = "🦄Dream Guy Xeon" //ur owner name
-global.ytname = "YT: Xeon" //ur yt chanel name
-global.socialm = "GitHub: DGXeon" //ur github or insta name
-global.location = "India, Mizoram, Aizawl" //ur location
+global.vcardowner = ['96171542228'] //ur owner number
+global.ownername = "TDM AMIR" //ur owner name
+global.ytname = "YT: Amircheayb" //ur yt chanel name
+global.socialm = "GitHub: Amircheayb8w6" //ur github or insta name
+global.location = "Lebanon, Baalbek, Nahle" //ur location
 
 //bot bomdy 
-global.owner = ['916909137213']
-global.ownertag = '916909137213' //ur tag number
+global.owner = ['96171542228']
+global.ownertag = '96171542228' //ur tag number
 global.botname = 'Cheems Bot MD V4' //ur bot name
 global.linkz = "https://youtu.be/WiIqCdiDjFo" //your theme url which will be displayed on whatsapp
 global.websitex = "https://youtu.be/WiIqCdiDjFo" //ur website to be displayed
-global.botscript = 'https://github.com/DGXeon/CheemsBot-MD4' //script link
-global.reactmoji = "😘" //ur menu react emoji
-global.themeemoji = "🦄" //ur theme emoji
-global.packname = "Cheems Bot Stickers\n\n\n\n\n\n\n\n\n\n\n🦄🦄🦄 " //ur sticker watermark packname
-global.author = "🦄Dream Guy Xeon" //ur sticker watermark author
+global.botscript = 'https://github.com/Amircheayb8w6/CheemsBot-MD4' //script link
+global.reactmoji = "😈" //ur menu react emoji
+global.themeemoji = "😈" //ur theme emoji
+global.packname = "Cheems Bot Stickers\n\n\n\n\n\n\n\n\n\n\n " //ur sticker watermark packname
+global.author = "TDM AMIR" //ur sticker watermark author
 
 //Bot theme media
 global.thum = fs.readFileSync("./XeonMedia/theme/cheemspic.jpg") //ur thumb pic
